a080b5e7c0ba39e16a23bd6eeb1ae92ab219bc41
nmare <nmare@DESKTOP-9VL7QGI>
2023-06-05 17:07:37 +0300

    Add project files.

---
+++ Registreerimine ja autoriseerimine/MyModule.py
@@ -0,0 +1,53 @@
+﻿from function import*
+
+
+def reg(login_list:list, pass_list:list):   
+    """
+    Регистрация новых пользователей,
+    Записывает в списки логины и пароли.
+    """
+    print('\n* Регистрация *'.center(24, ))
+
+    n = input('\nВведите логин: ')
+    u = username(n, login_list)
+    i = 0
+    while u == True:
+        n = input('Такой логин уже существует.\nВведите еще раз: ')
+        u = username(n, login_list)
+        i += 1
+        if i > 1:
+            print("Хотите завершить регистрацию? Напишите 1")
+            if n == '1':
+                print('Завершаем..')
+                return
+
+
+    v = input('Создать пароль автоматически или нет? 1/0: ')
+    if v == '1':
+        p = auto_pass()
+    else:
+        p = my_pass()
+
+    login_list.append(n)
+    pass_list.append(p)
+    print('\nРегистрация завершена'.center(33, )) 
+    
+
+
+def author(login_list:list, pass_list:list):
+    '''
+    Авторизирует пользователей, которые содержатся в списках.
+    '''
+    n = input('Введите логин: ')
+    u = username(n, login_list)
+    if u == True:
+        login = login_list.index(n)
+        n = input('Введите пароль: ')
+        if pass_list[login] == n:
+            return True
+        else:
+            print('--- Неправильный пароль ---')
+            return False
+    else:
+        print('--- Неправильный логин ---')
+        return False
---
+++ Registreerimine ja autoriseerimine/function.py
@@ -0,0 +1,59 @@
+﻿import random
+import re
+
+def username(n: str, l: list):
+    """
+    Ищет логин в списке, если находит, то возвращает истину (True).
+    Если не находит, то возвращает ложное значение (False).
+    """
+    if n in l:
+        t = True
+    else:
+        t = False
+    return t
+
+
+def auto_pass():
+    """
+    Функция, которая генерирует пароли автоматически.
+    """
+    str0 = "$@^=.,:;!_*-+()/#¤%&"
+    str1 = '0123456789'
+    str2 = 'qwertyuiopasdfghjklzxcvbnm'
+    str3 = str2.upper()
+    str4 = str0+str1+str2+str3
+    ls = list(str4)
+    random.shuffle(ls)
+    # Извлекаем из списка 12 произвольных значений
+    password = ''.join([random.choice(ls) for x in range(12)])
+    # Сгенирированный автоматически пароль, выводится строчкой ниже
+    print(f'Ваш пароль: {password}')
+    return password
+
+
+def my_pass():
+    """
+    Функция дает создать пароль самому.
+    Затем проверяет пароль на наличие цифр, малених и больших букв, спецсимволов.
+    """
+    print("Пароль должен состоять минимум из 8 символов, иметь мин. 1 цифру, 1 большую и маленькую букву и 1 спецсимвол")
+
+    i = 0
+    while True:
+        password = input('Введите пароль:')
+               
+        if len(password) < 8:
+            print('Пароль должен состоять минимум из 8 символов')
+        elif re.search('[0-9]', password) is None:
+            print('В пароле нет цифр')
+        elif re.search('[a-z]', password) is None:
+            print('В пароле нет букв в нижнем регистре')
+        elif re.search('[A-Z]', password) is None:
+            print('В пароле нет букв в верхнем регистре')
+        elif re.search('[$@^=.,:;!_*-+()/#¤%&]', password) is None:
+            print('В пароле нет спецсимволов')
+        else:
+            print('Ваш пароль надежный')
+            break
+
+    return password
